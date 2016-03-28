fe3137ca511de0d6ea13b62427967bbc430e6fa4
chetans <chetan.s@taboola.com>
2016-03-28 15:06:38 -0700

    New Changes made

--- src/linechart/DataSeries.jsx
+++ src/linechart/DataSeries.jsx
@@ -57,11 +57,11 @@ module.exports = React.createClass({
     var lines = props.data.map((series, idx) => {
         var color = "#ffffff";
         var gtSeriesName = series.name;
-        if(gtSeriesName == "Social"){
+        if(gtSeriesName == "facebook"){
             color = "#39579A";
-        }else if(gtSeriesName == "Search"){
+        }else if(gtSeriesName == "Google"){
             color = "#19A9E3";
-        }else if(gtSeriesName == "Direct"){
+        }else if(gtSeriesName == "Home"){
             color = "#E94435";
         }
 
